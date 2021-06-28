9d945c77e8550620aea782c71eb7aaf95b9a847e
yodai49 <cbasicjavaasembler@yahoo.co.jp>
2021-06-29 01:18:53 +0900

    successmessage

--- script.js
+++ script.js
@@ -733,12 +733,12 @@ document.getElementById("canvas").addEventListener("click", (e)=>{
                 
                 jQuery.ajax({
                     
-                    //type: 'get',
+                    //type: 'get',jj
                     dataType:"json",
                     data: {"twrite": "dedebug"}, //POSTするデータ
                     url: 'https://yodai49.github.io/cclemon/index.php', //送信先PHPファイル
                     success: function(){ //正常に処理が完了した時
-                        window.alert("success!");
+                        window.alert("success!!!");
                     }
                 });
 /*
