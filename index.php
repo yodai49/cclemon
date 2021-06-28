b788fd655ebc1b5605b3ed54acd51333ecbdd27e
yodai49 <cbasicjavaasembler@yahoo.co.jp>
2021-06-28 20:09:11 +0900

    check

--- index.php
+++ index.php
@@ -5,4 +5,6 @@ $newdate=$_POST["newdate"];
 $myfile = fopen("ranking.csv", "a");  //fileを上書きモードで開く
 fwrite($myfile, "debug1,debug2,debug3 \n". "\n");
 fclose($myfile);
+$alert = "<script type='text/javascript'>alert('aaa');</script>";
+echo $alert;
 ?>
