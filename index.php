b0a9a5ab25af27e9358bac0262e18e9e3b8fde3f
yodai49 <cbasicjavaasembler@yahoo.co.jp>
2021-06-29 00:32:22 +0900

    ranking fwritetest

--- index.php
+++ index.php
@@ -2,5 +2,6 @@
 $twrite=$_GET["twrite"];
 $myfile = fopen("https://yodai49.github.io/cclemon/ranking.csv", "a");  //fileを上書きモードで開く
 fwrite($myfile, $twrite);
+fwrite($myfile,"aaaaanoa");
 fclose($myfile);
 ?>
