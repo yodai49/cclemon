7ae9b7715e91e6c737e5e57eb0cb869fdb41a5de
yodai49 <cbasicjavaasembler@yahoo.co.jp>
2021-06-28 20:05:54 +0900

    debug

--- index.php
+++ index.php
@@ -1,28 +1,8 @@
-<!DOCTYPE html>
-
-<html lang="ja" dir="ltr">
-    <head>
-      <meta charset="utf-8">
-      <title></title>
-    </head>
-
-<body>
-
 <?php
 $newname=$_POST["newname"];
 $newrating=$_POST["newrating"];
 $newdate=$_POST["newdate"];
-?>
-
-<?php
-$myfile = fopen("ranking.csv", "a")  //fileを上書きモードで開く
-?>
-
-<?php fwrite($myfile, "debug1,debug2,debug3 \n". "\n"); ?>
-
-<?php
+$myfile = fopen("ranking.csv", "a");  //fileを上書きモードで開く
+fwrite($myfile, "debug1,debug2,debug3 \n". "\n");
 fclose($myfile);
 ?>
-
-</body>
-</html>
